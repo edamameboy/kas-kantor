c44c8a51d28e00e0bdb437383d8c240603d61600
edamameboy <sandyphang07@gmail.com>
2026-07-14 00:30:00 +0700

    migration to serverless

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,13 +1,9 @@
 import { PrismaClient } from '@prisma/client';
-import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
-
-// 1. Buat koneksi database menggunakan file SQLite
-const adapter = new PrismaBetterSqlite3({ url: './prisma/dev.db' });
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-// 3. Masukkan adapter ke dalam konstruktor PrismaClient
+// Biarkan kosong! Prisma secara otomatis akan menyedot `DATABASE_URL` dari .env atau Vercel
 export const prisma =
-  globalForPrisma.prisma || new PrismaClient({ adapter });
+  globalForPrisma.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
